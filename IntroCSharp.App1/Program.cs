6d64fd13cdb53a8d7c0005d2af80dfe5f99b9a91
leogrd49 <leogerard49.pro@gmail.com>
2024-02-19 10:32:28 +0100

    Base var, get entry

--- IntroCSharp.App1/Program.cs
+++ IntroCSharp.App1/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Baguette");
+﻿
+// Affiche Question
+Console.WriteLine("Comment tu t'appelles");
+
+//Get Reponse
+string? saisie = Console.ReadLine();
+
+//Print Bonjour + Reponse
+Console.WriteLine($"Bojour {saisie}");
